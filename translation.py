7b5c637ae9cb6c3339bafa530fa15ab60d8b1b61
hsl <jsnj_hangsile@outlook.com>
2023-12-28 22:51:03 +0800

    translation实现了单图片的翻译能力

--- translation.py
+++ translation.py
@@ -4,6 +4,9 @@ import uuid
 import requests
 import base64
 import hashlib
+import os
+import ast
+import binascii
 
 from imp import reload
 
@@ -36,9 +39,39 @@ class Translate():
         headers = {"Content-Type": "application/x-www-form-urlencoded"}
         return requests.post(Translate.YOUDAO_URL, data=data, headers=headers)
 
-    def connect(self):
+    # 转换算法，从16进制字符串utf-8变化为汉字
+    def utfTrans(string):
+        temp1 = string.encode("unicode_escape")
+        temp2 = temp1.decode("utf-8").replace("\\x",'')
+        result = binascii.a2b_hex(temp2).decode("utf-8")
+        return result
+
+    # 转换算法，将互联网2进制字典变为普通字典
+    def dictTrans(binary_dict):
+        # 转化为字符串形式的字典
+        temp = binary_dict.decode("utf-8")
+        return ast.literal_eval(temp)
+
+    # 根据网页返回结果，将识别出的原文与译文写入文本文档
+    def write_in_text(file_address, photo_address, binary_download_dict):
+        # 从网上获得的字典是二进制的，需要转化
+        dict = Translate.dictTrans(binary_download_dict)
+        # 获取没有扩展名的图片文件名
+        photo_name = os.path.basename(photo_address)
+        pre, back = os.path.splitext(photo_name)
+        # 写模式新建文件，如果已存在则报错
+        with open(pre + '.txt', 'w+', encoding="utf-8") as f:
+            for resRegion in dict["resRegions"]:
+                context = resRegion["context"]
+                f.write("原文：" + context + "\n")
+                tranContent = resRegion["tranContent"]
+                f.write("译文：" + tranContent + "\n")
+                f.write("\n")
+        f.close()
+
+    def connect(self, photo_address, file_address):
         # 以二进制方式打开图片
-        f = open(r"C:\Users\unicom_huangsl107\Desktop\1.png", "rb")
+        f = open(photo_address, "rb")
         # 读取文件内容并转换为base64编码
         q = base64.b64encode(f.read()).decode("utf-8")
         f.close()
@@ -56,15 +89,11 @@ class Translate():
         data["sign"] = sign
 
         response = Translate.do_request(data)
-        print(response.content)
+        Translate.write_in_text(file_address, photo_address, response.content)
 
-s1 = s.encode("unicode_escape")
-s2 = s1.decode("utf-8")
-s3 = s2.replace("\\x",'')
-s4 = binascii.a2b_hex(s3)
-s4.decode("utf-8")
+
 trans = Translate()
-trans.connect()
+trans.connect(r"C:\Users\hsl\source\repos\Syrzy\GITTEST\1.png",r"C:\Users\hsl\source\repos\Syrzy\GITTEST")
 
 
 def get_pictures():
